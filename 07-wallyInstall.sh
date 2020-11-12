79bda52de56649adf55c5a2c7242e5022d1ca01b
Gregor-Patrick Heine <heine.gregor@gmail.com>
2020-11-12 19:39:44 +0100

    easier install

--- 07-wallyInstall.sh
+++ 07-wallyInstall.sh
@@ -1,58 +1,76 @@
 #!/bin/bash
 set -e
 if [[ "$USER" == 'root' ]]; then
-    echo -e "\n\tPlease run script as non-root!\n"
+    echo -e "\n\tPlease run script as non-root user!\n"
     exit 1
+else
+    cd $HOME
 fi
 
-# INSTALL PACKAGES
-packageList='
-    gtk+3.0
-    libwebkit2gtk-4.0
-    libusb-dev
-    libusb-1.0
-'
-sudo apt-get install -y $packageList
+# GLOBALS
+goUrl='https://dl.google.com/go/go1.13.7.linux-armv6l.tar.gz'
+wallyBin='https://configure.ergodox-ez.com/wally/linux'
+wallyGit='github.com/zsa/wally-cli'
 
-# DOWNLOAD AND INSTALL GO-LANG
-curl https://dl.google.com/go/go1.13.7.linux-armv6l.tar.gz -O go.tar.gz
-sudo tar -C /usr/local -xzf go.tar.gz
+# INSTALL
+declare -A packageAA=(
+    ['apt-get']='
+        gtk+3.0
+        libwebkit2gtk-4.0
+        libusb-dev
+        libusb-1.0
+    '
+    ['yum']='
+        gtk3
+        webkit2gtk3
+        libusb
+    '
+    ['pacman']='
+        gtk3
+        webkit2gtk
+        libusb
+    '
+)
+for key in ${!packageAA[@]}; do
+    which $key && sudo $key install -y ${packageAA[$key]}
+done
 
-# SET PATHS
-cat <<\EOF >>$HOME/.bashrc
-alias wally='$HOME/go/bin/linux_arm64/wally-cli'
-export GOPATH=$HOME/go
-export PATH=/usr/local/go/bin:$PATH:$GOPATH/bin
-export CC=aarch64-linux-gnu-gcc
-export GOARCH=arm64
-export GOOS=linux
-export CGO_ENABLED=1
+# WALLY UDEV RULE FOR DEVICE RELATED EVENT
+cat <<EOF | sudo tee /etc/udev/rules.d/50-wally.rules
+# Teensy rules for the Ergodox EZ
+ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", ENV{ID_MM_DEVICE_IGNORE}="1"
+ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789A]?", ENV{MTP_NO_PROBE}="1"
+SUBSYSTEMS=="usb", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789ABCD]?", MODE:="0666"
+KERNEL=="ttyACM*", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", MODE:="0666"
+
+# STM32 rules for the Moonlander and Planck EZ
+SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", \
+    MODE:="0666", \
+    SYMLINK+="stm32_dfu"
 EOF
-. $HOME/.bashrc
 
-# BUILD WALLY
-go get -u github.com/zsa/wally-cli
-echo -e "Run:\n\twally <firmware-file>"
+# ADD USER TO PLUGDEV GROUP
+sudo groupadd plugdev
+sudo usermod -aG plugdev $USER
 
-
-# # WALLY UDEV RULE
-# cat <<EOF | sudo tee /etc/udev/rules.d/50-wally.rules
-# # Teensy rules for the Ergodox EZ
-# ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", ENV{ID_MM_DEVICE_IGNORE}="1"
-# ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789A]?", ENV{MTP_NO_PROBE}="1"
-# SUBSYSTEMS=="usb", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789ABCD]?", MODE:="0666"
-# KERNEL=="ttyACM*", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", MODE:="0666"
-
-# # STM32 rules for the Moonlander and Planck EZ
-# SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", \
-#     MODE:="0666", \
-#     SYMLINK+="stm32_dfu"
-# EOF
-
-# # ADD USER TO PLUGDEV GROUP
-# sudo groupadd plugdev
-# sudo usermod -aG plugdev $USER
-
-# # MESSAGE
-# echo -e "\n\tDone."
-
+# HARDWARE PLATFORM DEPENDENT WALLY
+if [[ "$(uname -i)" =~ 'x86' ]]; then
+    curl -OSL $wallyBin
+    sudo chmod +x wally
+    echo -e "You can launch the GUI via\n\t./wally "
+else
+    curl -SL $goUrl -O go.tar.gz
+    sudo tar -C /usr/local -xzf go.tar.gz
+    cat <<\EOF | sed -r 's/^ *//' >>$HOME/.bashrc
+        alias wally-cli='$HOME/go/bin/linux_arm64/wally-cli'
+        export GOPATH=$HOME/go
+        export PATH=/usr/local/go/bin:$PATH:$GOPATH/bin
+        export CC=aarch64-linux-gnu-gcc
+        export GOARCH=arm64
+        export GOOS=linux
+        export CGO_ENABLED=1
+EOF
+    . $HOME/.bashrc
+    go get -u $wallyGit
+    echo -e "Run:\n\twally-cli <firmware-file>"
+fi
